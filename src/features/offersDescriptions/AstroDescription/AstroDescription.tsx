f208771fef8138f6bde2bdc46cf741b027f04569
Alexandra M. <alexandra.v.mikh@gmail.com>
2025-08-16 11:20:21 +0100

    copies for products, product card as a separate component.

--- src/features/offersDescriptions/AstroDescription/AstroDescription.tsx
+++ src/features/offersDescriptions/AstroDescription/AstroDescription.tsx
@@ -1,11 +1,70 @@
 import { useTranslation } from 'react-i18next';
 import { ProductDescriptionCard } from "@/shared/ui/ProductDesriptionCard/ProductDescriptionCard";
+import styles from "@/features/offersDescriptions/styles/offersDescriptions.module.css";
+
+export interface Product {
+    id: number;
+    image: string;
+    title: string;
+    description: string;
+}
+
+const astroProducts: Product[] = [
+    {
+        id: 1,
+        image: "astroOfferDescr",
+        title: "astroProductTitle1",
+        description: "astroProductDescription1"
+    },
+    {
+        id: 2,
+        image: "astroOfferDescr",
+        title: "astroProductTitle2",
+        description: "astroProductDescription2"
+    },
+    {
+        id: 3,
+        image: "astroOfferDescr",
+        title: "astroProductTitle3",
+        description: "astroProductDescription3"
+    },
+    {
+        id: 4,
+        image: "astroOfferDescr",
+        title: "astroProductTitle4",
+        description: "astroProductDescription4"
+    } ,
+    {
+        id: 4,
+        image: "astroOfferDescr",
+        title: "astroProductTitle5",
+        description: "astroProductDescription5"
+    },
+    {
+        id: 4,
+        image: "astroOfferDescr",
+        title: "astroProductTitle6",
+        description: "astroProductDescription6"
+    }
+];
 
 export const AstroDescription = () => {
     const { t } = useTranslation();
 
     return(
-
-        <ProductDescriptionCard image={"astroOfferDescr"} title={t("astroOfferDescr")} description={t("astroOfferDescr")}/>
+        <div className={styles.container}>
+            <div className={styles.productsGrid}>
+                {astroProducts.map((product) => (
+                    <ProductDescriptionCard
+                        key={product.id}
+                        image={product.image}
+                        title={t(product.title)}
+                        description={t(product.description)}
+                    />
+                ))}
+            </div>
+        </div>
     );
 }
+
+
--- src/features/offersDescriptions/EventsDescription/EventsDescription.tsx
+++ src/features/offersDescriptions/EventsDescription/EventsDescription.tsx
@@ -1,27 +1,38 @@
 import styles from "@/features/offersDescriptions/styles/offersDescriptions.module.css";
-import {Icon} from "@/shared/ui/icons/Icon";
-import {scrollToElement} from "@/shared/lib/scrollToElement";
-import { useTranslation } from 'react-i18next';;
+import { useTranslation } from 'react-i18next';
+import type { Product } from '@/features/offersDescriptions/AstroDescription/AstroDescription';
+import { ProductDescriptionCard } from '@/shared/ui/ProductDesriptionCard/ProductDescriptionCard';
+
+const eventsProducts: Product[] = [
+    {
+        id: 1,
+        image: "eventsOfferDescr",
+        title: "eventsProductTitle1",
+        description: "eventsProductDescription1"
+    },
+    {
+        id: 2,
+        image: "eventsOfferDescr",
+        title: "eventsProductTitle2",
+        description: "eventsProductDescription2"
+    }
+]
 
 export const EventsDescription = () => {
     const { t } = useTranslation();
 
     return(
-        <div className={styles.activeCardDescrBlock}>
-            <span className={styles.activeCardDescrBlockPicture}><Icon name={"eventsOfferDescr"}/></span>
-
-            <p className={styles.activeCardDescrBlockText}>
-                {t("eventsOfferDescr")}
-            </p>
-
-            <div className={styles.activeCardDescrBlockButtons}>
-                <a href="https://ig.me/m/sasha.jyotika" target="_blank" rel="noopener noreferrer">
-                    <p>Message</p>
-                    <Icon name={"instagram"} size={25}/>
-                </a>
-                <button onClick={() => {scrollToElement("contactMe")}}>Email</button>
+        <div className={styles.container}>
+            <div className={styles.productsGrid}>
+                {eventsProducts.map((product) => (
+                    <ProductDescriptionCard
+                        key={product.id}
+                        image={product.image}
+                        title={t(product.title)}
+                        description={t(product.description)}
+                    />
+                ))}
             </div>
-
         </div>
     );
 }
