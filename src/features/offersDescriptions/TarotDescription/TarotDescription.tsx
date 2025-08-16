4c23ccab1260039da2f00c2d862c7e4c5b20a686
Alexandra M. <alexandra.v.mikh@gmail.com>
2025-08-16 11:32:28 +0100

    add copy for tarot products

--- src/features/offersDescriptions/TarotDescription/TarotDescription.tsx
+++ src/features/offersDescriptions/TarotDescription/TarotDescription.tsx
@@ -1,27 +1,38 @@
 import { useTranslation } from 'react-i18next';
 import styles from "@/features/offersDescriptions/styles/offersDescriptions.module.css";
-import {Icon} from "@/shared/ui/icons/Icon";
-import {scrollToElement} from "@/shared/lib/scrollToElement";
+import type { Product } from '@/features/offersDescriptions/AstroDescription/AstroDescription';
+import { ProductDescriptionCard } from '@/shared/ui/ProductDesriptionCard/ProductDescriptionCard';
+
+const tarotProducts: Product[] = [
+    {
+        id: 1,
+        image: "tarotOfferDescr",
+        title: "tarotProductTitle1",
+        description: "tarotProductDescription1"
+    },
+    {
+        id: 2,
+        image: "tarotOfferDescr",
+        title: "tarotProductTitle2",
+        description: "tarotProductDescription2"
+    }
+]
 
 export const TarotDescription = () => {
     const { t } = useTranslation();
 
     return(
-        <div className={styles.activeCardDescrBlock}>
-            <span className={styles.activeCardDescrBlockPicture}><Icon name={"tarotOfferDescr"}/></span>
-
-            <p className={styles.activeCardDescrBlockText}>
-                {t("tarotOfferDescr")}
-            </p>
-
-            <div className={styles.activeCardDescrBlockButtons}>
-                <a href="https://ig.me/m/sasha.jyotika" target="_blank" rel="noopener noreferrer">
-                    <p>Message</p>
-                    <Icon name={"instagram"} size={25}/>
-                </a>
-                <button onClick={() => {scrollToElement("contactMe")}}>Email</button>
+        <div className={styles.container}>
+            <div className={styles.productsGrid}>
+                {tarotProducts.map((product) => (
+                    <ProductDescriptionCard
+                        key={product.id}
+                        image={product.image}
+                        title={t(product.title)}
+                        description={t(product.description)}
+                    />
+                ))}
             </div>
-
         </div>
     );
 }
