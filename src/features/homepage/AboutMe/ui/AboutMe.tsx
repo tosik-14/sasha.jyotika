25ca6df832320c1dff8acbe95e5ba0c91a06f0e2
Alexandra M. <alexandra.v.mikh@gmail.com>
2025-08-19 21:28:47 +0100

    about me section with background, sticky nav fixed

--- src/features/homepage/AboutMe/ui/AboutMe.tsx
+++ src/features/homepage/AboutMe/ui/AboutMe.tsx
@@ -16,20 +16,21 @@ export const AboutMe = () => {
                     <Icon name={'sashaAboutMe'} size={[414, 532]} />
                 </span>
 
-                <div className={styles.aboutMeText}>
-                    <p className={styles.aboutMeGeneral}>{t('aboutMeGeneral')}</p>
-                    <span className={styles.aboutMeDetailText}>
-                        <p className={styles.aboutMeDetail}>
-                            {t('aboutMeDetail-1')}
-                        </p>
-                        <p className={styles.aboutMeDetail}>
-                            {t('aboutMeDetail-2')}
-                        </p>
-                        <p className={styles.aboutMeDetail}>
-                            {t('aboutMeDetail-3')}
-                        </p>
-                    </span>
-
+                <div className={styles.aboutMeTextContent}>
+                    <div className={styles.aboutMeText}>
+                        <p className={styles.aboutMeGeneral}>{t('aboutMeGeneral')}</p>
+                        <span className={styles.aboutMeDetailText}>
+                            <p className={styles.aboutMeDetail}>
+                                {t('aboutMeDetail-1')}
+                            </p>
+                            <p className={styles.aboutMeDetail}>
+                                {t('aboutMeDetail-2')}
+                            </p>
+                            <p className={styles.aboutMeDetail}>
+                                {t('aboutMeDetail-3')}
+                            </p>
+                        </span>
+                    </div>
                     <SocialMediaLinks />
                 </div>
             </div>
